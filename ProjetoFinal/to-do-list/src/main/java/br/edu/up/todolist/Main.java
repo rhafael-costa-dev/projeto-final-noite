11c6e7a1fa3b7ed0ce5e206a228b155b8f3690aa
Auto Logon <autologon@CSED.LAB>
2024-05-23 20:57:45 -0300

    Criação do menu principal da aplicação

--- ProjetoFinal/to-do-list/src/main/java/br/edu/up/todolist/Main.java
+++ ProjetoFinal/to-do-list/src/main/java/br/edu/up/todolist/Main.java
@@ -1,12 +1,49 @@
 package br.edu.up.todolist;
 
+import br.edu.up.todolist.utils.Util;
+import br.edu.up.todolist.views.ToDoListView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        logger.info("Testando");
-        System.out.println("Hello world!");
+        int op;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("###   MENU PRINCIPAL   ###");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Menu TO-DO-LIST");
+            System.out.println("2 - Menu Usuarios");
+
+            op = Util.lerInputInteiro(scanner);
+            exibirMenu(scanner, op);
+        } while (op != 0);
+        scanner.close();
     }
+
+    private static void exibirMenu(Scanner scanner, int op) {
+        switch (op) {
+            case 0:
+                System.out.println("Tchauuuu Obrigado!");
+                break;
+            case 1:
+                ToDoListView.exibirMenu(scanner);
+                break;
+            case 2:
+                ToDoListView.exibirMenu(scanner);
+                break;
+            case 99:
+                System.out.println("Opção invalida. Tente novamente!");
+                break;
+
+            default:
+                System.out.println("Opção invalida! Escolha uma opção valida de acordo com o Menu");
+                break;
+        }
+    }
+
 }
---
+++ ProjetoFinal/to-do-list/src/main/java/br/edu/up/todolist/utils/Util.java
@@ -0,0 +1,29 @@
+package br.edu.up.todolist.utils;
+
+import br.edu.up.todolist.Main;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Util {
+    private static final Logger logger = LogManager.getLogger(Util.class);
+
+    /**
+     * Método responsável por realizar a leitura do input de inteiros
+     * @param scanner
+     * @return
+     */
+    public static int lerInputInteiro(Scanner scanner) {
+        logger.info("Inicializando o processo de leitura de número inteiro!");
+        try {
+            int numero = scanner.nextInt();
+            return numero;
+        } catch (InputMismatchException ex) {
+            logger.error("Ocorreu um erro ao tentar ler um núemro inteiro", ex);
+            scanner.next();
+            return 99;
+        }
+    }
+}
